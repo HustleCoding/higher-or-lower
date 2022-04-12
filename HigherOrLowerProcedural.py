3005b11e3310684dca5a7a7ff85ce32dd720b456
HustleCoding <f.dobinciuc7@gmail.com>
2022-04-12 18:43:44 +0300

    First commit on the new branch

--- HigherOrLowerProcedural.py
+++ HigherOrLowerProcedural.py
@@ -1,5 +1,7 @@
 import random
 
+# This is a new branch
+
 # Card constants
 SUITS_TUPLE = ('Spades', 'Hearts', 'Clubs', 'Diamonds')
 RANK_TUPLE = ('Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King')
