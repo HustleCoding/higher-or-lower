0d18f5a0a74c75cef19ab3e32fa33107fc62f1f2
HustleCoding <f.dobinciuc7@gmail.com>
2022-04-12 15:38:25 +0300

    Create a function to get a card from deck

--- HigherOrLowerProcedural.py
+++ HigherOrLowerProcedural.py
@@ -1,4 +1,14 @@
 import random
 
+# Card constants
 SUITS_TUPLE = ('Spades', 'Hearts', 'Clubs', 'Diamonds')
 RANK_TUPLE = ('Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King')
+
+NCARDS = 8
+
+# Pass in a deck and this function returns a random card from the deck
+def getCard(deckListIn):
+    thisCard = deckListIn.pop() # pop one off the top of the deck and return
+    return thisCard
+
+
